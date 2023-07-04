e8cb35186be1ff09ff8d2902bb4bd9f0da9770c9
Siddhartha Pramanik <spramanik@phdata.io>
2023-07-04 17:23:26 +0530

    fix __init__.py to send correcct status code and add .config file

--- az-container-start-func/start-container/__init__.py
+++ az-container-start-func/start-container/__init__.py
@@ -1,26 +1,34 @@
 import logging
+import configparser
+import os
 
 import azure.functions as func
 from azure.identity import DefaultAzureCredential
-from start_az_container import start_container
+from . import start_az_container
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+
     logging.info('Python HTTP trigger function processed a request.')
 
-    subscription_id = '983479f6-025c-4bd4-a886-3a33d5eb26eb'
-    resource_group = 'rg_dbtOnAzure'
+    config = configparser.ConfigParser()
+    script_dir = os.path.dirname(__file__)
+    config_file = os.path.join(script_dir,'credentials.config')
+    config.read(config_file)
+    subscription_id = config['AZURE']['subscription_id']
+    resource_group = config['AZURE']['resource_group']
 
     credential = DefaultAzureCredential()
-    
+	
     container_name = req.params.get('container_name')
 
     if container_name:
         try:
-            start_container(subscription_id, credential, resource_group, container_name)
-            return func.HttpResponse("Container successfully started", status_Code=200)
+            start_az_container.start_container(subscription_id, credential, resource_group, container_name)
+            return func.HttpResponse("Container successfully started", status_code=200)
         except:
-            return func.HttpResponse("Issue with container start", status_Code=500)
+            return func.HttpResponse("Issue with container start", status_code=500)
     
     else:
-        return func.HttpResponse("Pass container_name in request", status_Code=400)
+        return func.HttpResponse("Pass container_name in request", status_code=400)
+        
